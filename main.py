9d6d8ed169e3ef1b0efb2c25c0442204656f1a64
elson1608 <pinter.elias2001@gmail.com>
2023-11-18 16:10:07 +0100

    generated instances

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from instance import Instance, assign_new_costs
-from solve import solve_cplex, solve_NuWLS
+from instances.instance import Instance, generate
+from solvers.nuwls import NuWLSSolver
 import time
 
 if __name__ == "__main__":
      # assign_new_costs('rail516', 0.1)
      # assign_new_costs('sts-9', 0.1)
-     assign_new_costs('scpnre1', 0.1)
+     # assign_new_costs('scpnre1', 0.1)
      # assign_new_costs('scpcyc-06', 0.1)
      # assign_new_costs('scpclr-10', 0.1)
      # assign_new_costs('scp41', 0.1)
-     # instance = Instance('scpcyc-06')
+     # instance = Instance('sts-9')
+     # solver = NuWLSSolver()
+     # elapsed_time, solution_cost = solver.solve(instance, 10)
+     # print('done')
      # solve_NuWLS(instance, 10)
      # solve_cplex(instance)
 
-     # instance = Instance('AAS-Weighted-Set-Cover/instances/sts/data.135')
+     instance = Instance('scp42')
+     instance.assign_new_costs_margin(0.5)
      # instance = Instance('AAS-Weighted-Set-Cover/instances/scp-clr/scpclr10.txt')
      # instance = Instance('AAS-Weighted-Set-Cover/instances/scp-cyc/scpcyc06.txt')
-     pass
+     
